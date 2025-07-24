b1e5dc55fd9e364357ec7ad28b96ab8c3d46a0cd
mkumm14 <madhavkummara@outlook.com>
2025-07-24 01:05:52 -0500

    Add a static class (ImageDownloader) to download images using downloadUrl

---
+++ Utils/ImageDownloader.cs
@@ -0,0 +1,18 @@
+﻿namespace ExpenseClaimProject.Utils
+{
+    public static class ImageDownloader
+    {
+
+        public static async Task<byte[]> DownloadImageBytesAsync(HttpClient httpClient, string imageUrl)
+        {
+
+            using var resp = await httpClient.GetAsync(imageUrl);
+            resp.EnsureSuccessStatusCode();
+            return await resp.Content.ReadAsByteArrayAsync();
+
+        }
+
+
+
+    }  
+}
